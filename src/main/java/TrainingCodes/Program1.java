d6c8d89abf99d22d4a57175b57379403ec06eb79
Dell <Dell@SAM>
2023-03-21 10:47:47 +0530

    Old Programs added

---
+++ src/main/java/TrainingCodes/Program1.java
@@ -0,0 +1,17 @@
+package TrainingCodes;
+/**
+ * 
+ */
+        
+/**
+ * @author Dell
+ *
+ */
+public class Program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println();
+	}
